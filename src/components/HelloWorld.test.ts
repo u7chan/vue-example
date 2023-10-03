3638c757fd8598c2b86c79b4986e5bf78ed02d49
una <unagamix@gmail.com>
2023-10-03 23:06:35 +0900

    fix: add test case

--- src/components/HelloWorld.test.ts
+++ src/components/HelloWorld.test.ts
@@ -11,4 +11,15 @@ describe('HelloWorld', () => {
     })
     expect(wrapper.text()).toContain('hello')
   })
+
+  test('should count up from 0 to 1', async () => {
+    const wrapper = mount(HelloWorld, {
+      props: {
+        msg: 'hello',
+      },
+    })
+    expect(wrapper.text()).toContain('count is 0')
+    await wrapper.find('button').trigger('click')
+    expect(wrapper.text()).toContain('count is 1')
+  })
 })
